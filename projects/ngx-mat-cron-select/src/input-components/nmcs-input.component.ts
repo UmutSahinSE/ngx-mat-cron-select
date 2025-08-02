b8a963033a269f29cdaf47878a588d8977d6cce9
Umut Sahin <umut.sahin@supplychainwizard.com>
2025-08-02 19:24:04 +0300

    Added validation for nmcs inputs

--- projects/ngx-mat-cron-select/src/input-components/nmcs-input.component.ts
+++ projects/ngx-mat-cron-select/src/input-components/nmcs-input.component.ts
@@ -1,4 +1,4 @@
-import { computed, Directive, effect, forwardRef, input, InputSignal, output, signal } from '@angular/core';
+import { computed, Directive, effect, forwardRef, input, InputSignal, output } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { ControlValueAccessor, FormControl, NG_VALUE_ACCESSOR, TouchedChangeEvent, Validators } from '@angular/forms';
 import { BehaviorSubject, filter, map, Observable, startWith, switchMap } from 'rxjs';
@@ -18,11 +18,10 @@ export type TNmcsValue = TNmcsMultiSelectValue | TNmcsSingleSelectValue;
 })
 export abstract class NmcsInput<FormControlValue extends TNmcsValue> implements ControlValueAccessor {
   public formControl: InputSignal<FormControl<FormControlValue>> = input<FormControl<FormControlValue>>(
-    // @ts-ignore
     new FormControl<number[]>([], {
       nonNullable: true,
       validators: [Validators.required],
-    }),
+    }) as FormControl<FormControlValue>,
   );
   protected isInitialized = false;
   protected previousValue: FormControlValue | undefined;
@@ -35,7 +34,6 @@ export abstract class NmcsInput<FormControlValue extends TNmcsValue> implements 
     new Observable<FormControlValue>(),
   );
   public readonly isMultiselect = computed(() => Array.isArray(this.formControl().value));
-  public readonly isDisabled = signal(false);
 
   protected constructor() {
     this.registerInputFormControlEvents();
@@ -79,11 +77,10 @@ export abstract class NmcsInput<FormControlValue extends TNmcsValue> implements 
   }
 
   public writeValue(value: FormControlValue): void {
-    // TODO validate
-    // check single select form group vs value
+    const valueAfterValidation = this.validateInput(value);
 
-    if (this.formControl().value !== value) {
-      this.formControl().reset(value);
+    if (this.formControl().value !== valueAfterValidation) {
+      this.formControl().reset(valueAfterValidation);
     }
   }
 
@@ -95,12 +92,9 @@ export abstract class NmcsInput<FormControlValue extends TNmcsValue> implements 
     this.onTouched = fn;
   }
 
-  public setDisabledState(isDisabled: boolean): void {
-    // TODO use isDisabled
-    this.isDisabled.set(isDisabled);
-  }
-
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   private onChange = (_value: FormControlValue): void => {};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   private onTouched = (): void => {};
 
   private registerInputFormControlEvents(): void {
@@ -119,5 +113,13 @@ export abstract class NmcsInput<FormControlValue extends TNmcsValue> implements 
       );
       this.inputsFormGroupValueAdapter.next(formControl.valueChanges.pipe(startWith(formControl.value)));
     });
+  }
+
+  private validateInput(value: FormControlValue): FormControlValue {
+    const isMulti = Array.isArray(this.formControl().value);
+
+    const isValid = isMulti ? Array.isArray(value) : !Array.isArray(value);
+
+    return isValid ? value : ((isMulti ? [] : null) as FormControlValue);
   }
 }
